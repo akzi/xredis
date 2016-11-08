b0c138b31a8c1c8021e83a221de73ecc2d161840
akzi <382018309@qq.com>
2016-11-08 16:05:02 +0100

    add codes

--- include/detail/reply_parser.hpp
+++ include/detail/reply_parser.hpp
@@ -1,5 +1,5 @@
 #pragma once
-/*
+/* redis : cluster slots
 *2
 	*5
 		:4000
@@ -55,12 +55,14 @@ namespace xredis
 			{
 				enum type_t
 				{
+					e_cluster_slots_cb,
 					e_str_map_cb,
 					e_integral_cb,
 					e_bulk_cb,
 				} type_;
 				union
 				{
+					cluster_slots_callback *cluster_slots_cb_;
 					string_map_callback *bulk_map_cb_;
 					string_callback *bulk_cb_;
 					integral_callback *int_cb_;
@@ -78,7 +80,17 @@ namespace xredis
 			struct obj
 			{
 				obj() {}
-				~obj() {}
+				~obj() 
+				{
+					if(type_ == obj::e_array)
+					{
+						for(auto &itr : *array_)
+							delete itr;
+						delete array_;
+					}
+					else if(type_ == obj::e_bulk)
+						delete str_;
+				}
 				enum type_t
 				{
 					e_new,
@@ -121,13 +133,7 @@ namespace xredis
 					memset(&obj_, 0, sizeof obj_);
 				}
 			};
-			void regist_callback(string_map_callback cb)
-			{
-				callback tmp;
-				tmp.bulk_map_cb_ = new string_map_callback(cb);
-				tmp.type_ = callback::e_str_map_cb;
-				callbacks_.emplace_back(tmp);
-			}
+			
 		public:
 			reply_parser()
 			{
@@ -142,6 +148,11 @@ namespace xredis
 			{
 
 			}
+			template<typename CB>
+			void regist_callback(CB &&cb)
+			{
+				append_cb(std::move(cb));
+			}
 		private:
 			void run()
 			{
@@ -149,7 +160,8 @@ namespace xredis
 				{
 					if (task_.parser_funcs_.empty())
 					{
-						prepare();
+						if(prepare(task_.obj_) == false)
+							return;
 					}
 					while (task_.parser_funcs_.size())
 					{
@@ -165,51 +177,50 @@ namespace xredis
 				} while (true);
 				
 			}
-			bool prepare()
+			bool prepare(obj &o)
 			{
+				if(pos_ >= data_.size())
+					return false;// no data
+
 				if(task_.parser_funcs_.empty())
 					task_.parser_funcs_.emplace_back(
 					std::bind(&reply_parser::parser_done, this));
-
-				if(pos_ >= data_.size())
-					return false;// no data
 
 				char ch = data_[pos_];
 				switch (ch)
 				{
 				case ':':
 					task_.parser_funcs_.emplace_back(
-						std::bind(&reply_parser::get_integal,this));
+						std::bind(&reply_parser::get_integal,this, std::ref(o)));
 					break;
 				case '$':
 					task_.parser_funcs_.emplace_back(
-						std::bind(&reply_parser::get_bulk, this));
+						std::bind(&reply_parser::get_bulk, this, std::ref(o)));
 					task_.parser_funcs_.emplace_back(
-						std::bind(&reply_parser::get_len, this));
+						std::bind(&reply_parser::get_len, this, std::ref(o)));
 					break;
 				case '*':
 					task_.parser_funcs_.emplace_back(
-						std::bind(&reply_parser::get_array, this));
+						std::bind(&reply_parser::get_array, this, std::ref(o)));
 					task_.parser_funcs_.emplace_back(
-						std::bind(&reply_parser::get_len, this));
+						std::bind(&reply_parser::get_len, this, std::ref(o)));
 					break;
 				case '-':
 					task_.obj_.type_ = obj::e_error;
 					task_.parser_funcs_.emplace_back(
-						std::bind(&reply_parser::get_str, this));
+						std::bind(&reply_parser::get_str, this, std::ref(o)));
 					break;
 				case '+':
 					task_.obj_.type_ = obj::e_ok;
 					task_.parser_funcs_.emplace_back(
-						std::bind(&reply_parser::get_str, this));
+						std::bind(&reply_parser::get_str, this, std::ref(o)));
 					break;
 				}
 				++pos_;
 				return true;
 			}
-			bool get_str()
+			bool get_str(obj &o)
 			{
-				obj &o = get_obj();
 				if (!o.str_)
 					o.str_ = new std::string;
 
@@ -227,17 +238,16 @@ namespace xredis
 				}
 				return false;
 			}
-			bool get_bulk()
+			bool get_bulk(obj &o)
 			{
-				obj &o = get_obj();
 				if(pos_ + o.len_ > data_.size())
 					return false;
 				o.str_ = new std::string(data_.data() + pos_, o.len_);
-				pos_ += o.len_;
+				pos_ += o.len_ + 2;//\r\n
 				o.type_ = obj::e_bulk;
 				return true;
 			}
-			bool get_len()
+			bool get_len(obj &o)
 			{
 				set_roll_back();
 				std::string buf;
@@ -249,7 +259,7 @@ namespace xredis
 					else if (data_[pos_ + 1] == '\n')
 					{
 						pos_ += 2;
-						get_obj().len_ = std::strtol(buf.c_str(), 0, 10);
+						o.len_ = std::strtol(buf.c_str(), 0, 10);
 						if (errno == ERANGE)
 							assert(false);
 						return true;
@@ -259,7 +269,7 @@ namespace xredis
 				roll_back();
 				return false;
 			}
-			bool get_integal()
+			bool get_integal(obj &o)
 			{
 				set_roll_back();
 				std::string buf;
@@ -271,7 +281,6 @@ namespace xredis
 					else if (data_[pos_ + 1] == '\n')
 					{
 						pos_ += 2;
-						obj &o = get_obj();
 						o.integral_ = std::strtol(buf.c_str(), 0, 10);
 						if (errno == ERANGE)
 							assert(__FILE__ == 0);
@@ -283,25 +292,34 @@ namespace xredis
 				roll_back();
 				return false;
 			}
-			bool get_array()
+			bool get_array(obj &o)
 			{
-				obj &o = get_obj();
 				int elememts = o.len_;
 				if (elememts == -1)
 				{
 					o.type_ = obj::e_null;
 					return true;
 				}
+				o.array_ = new std::vector<obj*>;
+				o.type_ = obj::e_array;
 				for (int i = 0; i < elememts; ++i)
 				{
+					obj *tmp = new obj;
+					o.array_->push_back(tmp);
 					task_.parser_funcs_.emplace_back(
-						std::bind(&reply_parser::prepare, this));
+						std::bind(&reply_parser::prepare, this, std::ref(*tmp)));
 				}
 				o.type_ = obj::e_array;
 				return true;
 			}
 			bool reset_task()
 			{
+				if(pos_ == data_.size())
+					data_.clear();
+				else
+				{
+					data_ = data_.substr(pos_, data_.size() - pos_);
+				}
 				task_.reset();
 				return true;
 			}
@@ -313,27 +331,7 @@ namespace xredis
 			{
 				pos_ = last_pos_;
 			}
-			obj &get_obj()
-			{
-				if(task_.obj_.type_ != obj::e_array)
-					return task_.obj_;
 
-				if (!task_.obj_.array_)
-					task_.obj_.array_ = new std::vector<obj *>;
-
-				if (task_.obj_.array_->empty())
-					goto new_obj;
-
-				if (task_.obj_.array_->back()->type_ != obj::e_new)
-					goto new_obj;
-				else
-					return *task_.obj_.array_->back();
-
-			new_obj:
-				obj *o = new obj;
-				task_.obj_.array_->push_back(o);
-				return *o;
-			}
 			bool parser_done()
 			{
 				assert(callbacks_.size());
@@ -347,10 +345,56 @@ namespace xredis
 				case callback::e_str_map_cb:
 					str_map_cb(cb);
 					break;
+				case callback::e_cluster_slots_cb:
+					cluster_slots_cb(cb);
 				default:
 					break;
 				}
+				reset_task();
 				return true;
+			}
+			void cluster_slots_cb(callback cb)
+			{
+				obj &o = task_.obj_;
+				if(o.type_ == obj::e_null)
+					(*cb.cluster_slots_cb_)("null", { });
+				else if(o.type_ == obj::e_error)
+					(*cb.cluster_slots_cb_)(std::move(*o.str_), { });
+				else if(task_.obj_.type_ != obj::e_array)
+					(*cb.cluster_slots_cb_)("error", { });
+
+				cluster_slots slots;
+				for(std::size_t i = 0; i < o.array_->size(); i++)
+				{
+					obj &sub_ = *(*o.array_)[i];
+					if(sub_.type_ != obj::e_array)
+						(*cb.cluster_slots_cb_)("error", { });
+
+					cluster_slots::cluster_info info;
+					std::size_t size = sub_.array_->size();
+					if(size < 3)
+						goto error;
+					info.min_slots_ = (*(*sub_.array_)[size -1]).integral_;
+					info.max_slots_ = (*(*sub_.array_)[size - 2]).integral_;
+					for(int k = (int)size-3; k >= 0;)
+					{
+						cluster_slots::cluster_info::node node;
+						obj &n = *(*sub_.array_)[k--];
+						if(n.type_ != obj::e_array)
+							goto error;
+						node.id_ = std::move( *(*(*n.array_)[0]).str_);
+						node.port_ = (*(*n.array_)[1]).integral_;
+						node.ip_ = std::move(*(*(*n.array_)[2]).str_);
+						info.node_.emplace_back(std::move(node));
+					}
+					slots.cluster_.emplace_back(std::move(info));
+				}
+				(*cb.cluster_slots_cb_)("error", std::move(slots));
+				return;
+
+			error:
+				(*cb.cluster_slots_cb_)("error", { });
+				return;
 			}
 			void str_map_cb(const callback &cb)
 			{
@@ -365,7 +409,8 @@ namespace xredis
 					assert(o.array_->size()%2 == 0);
 					for (std::size_t i =0; i < o.array_->size();)
 					{
-						result.emplace(std::move(*(*o.array_)[i]->str_), std::move(*(*o.array_)[i+1]->str_));
+						result.emplace(std::move(*(*o.array_)[i]->str_), 
+									   std::move(*(*o.array_)[i+1]->str_));
 						i += 2;
 					}
 				}
@@ -373,11 +418,18 @@ namespace xredis
 			}
 			void string_cb(const callback &cb)
 			{
-				obj &o = get_obj();
+				obj &o = task_.obj_;
 				if(o.type_ == obj::e_null)
 					(*cb.bulk_cb_)("null", "");
 				(*cb.bulk_cb_)("",std::move(*o.str_));
 				reset_task();
+			}
+			void append_cb(cluster_slots_callback  &&cb)
+			{
+				callback tmp;
+				tmp.cluster_slots_cb_= new cluster_slots_callback(std::move(cb));
+				tmp.type_ = callback::e_cluster_slots_cb;
+				callbacks_.emplace_back(tmp);
 			}
 			std::string::size_type last_pos_ = 0;
 			std::string::size_type pos_ = 0;
--- test/reply_parser_test.cpp
+++ test/reply_parser_test.cpp
@@ -4,18 +4,29 @@ XTEST_SUITE(reply_parser)
 {
 	XUNIT_TEST(parse)
 	{
-		const std::string buffer = 
-					"*2\r\n"
-					"*5\r\n:4000\r\n:16383\r\n"
-					"*3\r\n$13\r\n192.168.3.224\r\n:7000\r\n$40\r\nb9ab1cef8c926f9771fd4efa16d047391a179e4d\r\n"
-					"*3\r\n$13\r\n192.168.3.224\r\n:7002\r\n$40\r\n232eb1f44c2860a2b17b9d1cc2d3cd5854a728aa\r\n"
-					"*3\r\n$13\r\n192.168.3.224\r\n:7001\r\n$40\r\nc6d8894dc6b281488185a4734578eda2cf80442b\r\n"
-					"*5\r\n:0\r\n:3999\r\n"
-					"*3\r\n$13\r\n192.168.3.224\r\n:7003\r\n$40\r\n138eb855c041bbe2b8bb72e6f38c3a2d17697884\r\n"
+		const std::string buffer =
+			"*2\r\n"
+			"*5\r\n:4000\r\n:16383\r\n"
+			"*3\r\n$13\r\n192.168.3.224\r\n:7000\r\n$40\r\nb9ab1cef8c926f9771fd4efa16d047391a179e4d\r\n"
+			"*3\r\n$13\r\n192.168.3.224\r\n:7002\r\n$40\r\n232eb1f44c2860a2b17b9d1cc2d3cd5854a728aa\r\n"
+			"*3\r\n$13\r\n192.168.3.224\r\n:7001\r\n$40\r\nc6d8894dc6b281488185a4734578eda2cf80442b\r\n"
+			"*5\r\n:0\r\n:3999\r\n"
+			"*3\r\n$13\r\n192.168.3.224\r\n:700";
+		const std::string buffer2 = "3\r\n$40\r\n138eb855c041bbe2b8bb72e6f38c3a2d17697884\r\n"
 					"*3\r\n$13\r\n192.168.3.224\r\n:7005\r\n$40\r\n308d41acf591ec905c577e997942fcded12882be\r\n"
 					"*3\r\n$13\r\n192.168.3.224\r\n:7004\r\n$40\r\n0fcfcfdbd9228b3d90f3f2dc461447ae037091ba\r\n";
 
 		xredis::detail::reply_parser parser;
+
+		xredis::detail::cluster_slots_callback cb = 
+			[](std::string && status, xredis::detail::cluster_slots &&cluster_slots) 
+		{ 
+
+		};
+
+		parser.regist_callback(cb);
+
 		parser.parse(buffer.data(), buffer.size());
+		parser.parse(buffer2.data(), buffer2.size());
 	}
 }
